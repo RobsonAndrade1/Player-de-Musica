c2b1c94e5258f3223b8581c96812c66eb0bbdb04
RobsonAndrade1 <andrade.rob1691@gmail.com>
2022-11-14 21:24:27 -0300

    barra de tempo

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ let musica = document.querySelector("audio");
 
 document.querySelector(".botao-play").addEventListener("click", tocarMusica);
 document.querySelector(".botao-pause").addEventListener("click", pausarMusica);
+musica.addEventListener("timeupdate", atualizarBarra);
 
 function tocarMusica() {
     musica.play();
@@ -13,4 +14,11 @@ function pausarMusica() {
     musica.pause();
     document.querySelector(".botao-pause").style.display = "none";
     document.querySelector(".botao-play").style.display = "block";
+}
+
+function atualizarBarra() {
+    let barra = document.querySelector("progress");
+    barra.style.width = Math.floor((musica.currentTime / musica.duration) * 100) + "%";
+    let tempoDecorrido = document.querySelector(".inicio");
+    tempoDecorrido.textContent = Math.floor(musica.currentTime);
 }
